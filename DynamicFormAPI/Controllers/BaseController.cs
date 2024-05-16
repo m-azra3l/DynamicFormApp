a1d55c2192ed416d8f114021004e19f10a741287
Michael <gadreelazazel@gmail.com>
2024-05-16 11:13:27 +0100

    Update Setup base controller

---
+++ DynamicFormAPI/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicFormAPI.Controllers
+{
+    [Route("api/v1/")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+    }
+}
